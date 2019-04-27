cfafc30948b35eb0c1009dc76bb1ace100cb34cc
Brenden Matthews <brenden@diddyinc.com>
2019-04-27 13:21:17 -0400

    More API WIP.

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,7 @@ extern crate log;
 extern crate failure;
 
 use rocket::response::content;
-use rocket_contrib::json::JsonValue;
-use rocket_cors;
-use rocket_cors::{AllowedHeaders, AllowedOrigins, Error};
+use rocket_contrib::json::{Json, JsonValue};
 
 //  type Buf = Buffer<AddOrigin<Connection<TcpStream, DefaultExecutor, BoxBody>>, http::Request<BoxBody>>;
 //  type RolodexClient = client::RolodexService<Buf>;
@@ -29,66 +27,68 @@ mod certs;
 mod config;
 mod rolodex_client;
 
-#[derive(Debug, Responder)]
-#[response(status = 500, content_type = "json")]
-pub struct ResponseError {
-    response: content::Json<String>,
+#[derive(Responder, Debug)]
+enum ResponseError {
+    #[response(status = 500, content_type = "json")]
+    InternalError { response: content::Json<String> },
+    #[response(status = 404, content_type = "json")]
+    NotFound { response: content::Json<String> },
+    #[response(status = 400, content_type = "json")]
+    BadRequest { response: content::Json<String> },
+    #[response(status = 403, content_type = "json")]
+    Unauthorized { response: content::Json<String> },
 }
 
-#[derive(Debug, Responder)]
-#[response(status = 404, content_type = "json")]
-pub struct ResponseNone {
-    response: content::Json<String>,
+#[derive(Debug, Serialize)]
+struct AuthResponse {
+    token: String,
 }
 
-#[derive(Debug, Responder)]
-#[response(status = 400, content_type = "json")]
-pub struct ResponseBadRequest {
-    response: content::Json<String>,
-}
-
-#[get("/")]
-fn hello() -> Result<content::Json<String>, ResponseError> {
-    use rolodex_grpc::proto::*;
+#[get("/", format = "json")]
+fn hello() -> Result<Json<AuthResponse>, ResponseError> {
     let rolodex_client = rolodex_client::Client::new(&config::CONFIG);
-    let result = rolodex_client.add_user(NewUserRequest {
+    let result = rolodex_client.add_user(rolodex_grpc::proto::NewUserRequest {
         full_name: "What is in a name?".to_string(),
         email: "hey poo".to_string(),
         password_hash: "123".to_string(),
-        phone_number: Some(PhoneNumber {
+        phone_number: Some(rolodex_grpc::proto::PhoneNumber {
             country: "US".into(),
             number: "123".into(),
         }),
     });
-    match result {
-        Ok(resp) => match resp.result {
-            Some(result) => match result {
-                new_user_response::Result::UserId(user_id) => Ok(content::Json(
-                    json!({
-                        "user_id":user_id.clone(),
-                    })
-                    .to_string(),
-                )),
-                new_user_response::Result::Error(err) => Ok(content::Json(
-                    json!({
-                        "error":err,
-                    })
-                    .to_string(),
-                )),
-            },
-            None => Err(ResponseError {
-                response: content::Json(
-                    json!({
-                        "error": "Unauthorized"
-                    })
-                    .to_string(),
-                ),
-            }),
-        },
-        Err(err) => Err(ResponseError {
-            response: content::Json(json!({ "error": err.to_string() }).to_string()),
-        }),
-    }
+    // match result {
+    //     Ok(resp) => match resp.result {
+    //         Some(result) => match result {
+    //             new_user_response::Result::UserId(user_id) => Ok(content::Json(
+    //                 json!({
+    //                     "user_id":user_id.clone(),
+    //                 })
+    //                 .to_string(),
+    //             )),
+    //             new_user_response::Result::Error(err) => Ok(content::Json(
+    //                 json!({
+    //                     "error":err,
+    //                 })
+    //                 .to_string(),
+    //             )),
+    //         },
+    //         None => Err(ResponseError::Unauthorized {
+    //             response: content::Json(
+    //                 json!({
+    //                     "error": "Unauthorized"
+    //                 })
+    //                 .to_string(),
+    //             ),
+    //         }),
+    //     },
+    //     Err(err) => Err(ResponseError::InternalError {
+    //         response: content::Json(json!({ "error": err.to_string() }).to_string()),
+    //     }),
+    // }
+
+    Ok(Json(AuthResponse {
+        token: "lol".into(),
+    }))
 }
 
 #[catch(404)]
@@ -107,18 +107,45 @@ fn unprocessable_entity() -> JsonValue {
     })
 }
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), std::io::Error> {
+    use rocket_cors::{AllowedHeaders, AllowedOrigins};
+use rocket_contrib::compression::Compression;
+
     config::load_config();
 
-    let cors = rocket_cors::Cors {
+    use std::str::FromStr;
+    use rocket_cors::AllowedMethods;
+
+let allowed_methods: AllowedMethods = ["Get", "Post", "Put", "Delete"]
+   .iter()
+   .map(|s| FromStr::from_str(s).unwrap())
+   .collect();
+
+    let cors = rocket_cors::CorsOptions {
         allowed_origins: AllowedOrigins::all(),
+        allowed_methods,
         allowed_headers: AllowedHeaders::all(),
         allow_credentials: true,
         ..Default::default()
-    };
+    }
+    .to_cors()
+    .unwrap();
+
+    // use rocket::http::uri::Uri;
+    // use rocket_contrib::helmet::{Frame, Hsts, NoSniff, SpaceHelmet, XssFilter};
+
+    // let site_uri = Uri::parse("https://mysite.example.com").unwrap();
+    // let report_uri = Uri::parse("https://report.example.com").unwrap();
+    // let helmet = SpaceHelmet::default()
+    //     .enable(Hsts::default())
+    //     .enable(Frame::AllowFrom(site_uri))
+    //     .enable(XssFilter::EnableReport(report_uri))
+    //     .disable::<NoSniff>();
 
     rocket::ignite()
+        // .attach(helmet)
         .attach(cors)
+        .attach(Compression::fairing())
         .register(catchers![not_found, unprocessable_entity])
         .mount("/", routes![hello])
         .launch();
