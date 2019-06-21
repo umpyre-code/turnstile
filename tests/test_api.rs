8a84eef0a2caf543ffce1eb648ad22d83883559b
Brenden Matthews <brenden@diddyinc.com>
2019-06-21 17:47:49 -0400

    Revert API test URL.

--- tests/test_api.rs
+++ tests/test_api.rs
@@ -29,8 +29,7 @@ impl Turnstile {
 
         // Get next available
         let port = GLOBAL_PORT_COUNTER.fetch_add(1, Ordering::SeqCst);
-        let url = "https://api.staging.umpyre.io".to_string();
-        // let url = format!("http://localhost:{}", port);
+        let url = format!("http://localhost:{}", port);
 
         // Fork binary to background
         Turnstile {
