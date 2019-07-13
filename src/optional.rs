6d4b54d61e8cadf37db8271f00c49220801ee922
Brenden Matthews <brenden@diddyinc.com>
2019-07-13 14:30:08 -0400

    Add missing file.

---
+++ src/optional.rs
@@ -0,0 +1,17 @@
+pub trait Optional
+where
+    Self: std::marker::Sized,
+{
+    fn into_option(&self) -> Option<Self>;
+}
+
+impl Optional for String where
+    Self: std::marker::Sized, {
+     fn into_option(&self) -> Option<Self> {
+        if self.is_empty() {
+            None
+        } else {
+            Some(self.clone())
+        }
+    }
+}
