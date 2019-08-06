da7e227b6cf19891c7c4436944d529cce297213a
Brenden Matthews <brenden@diddyinc.com>
2019-08-06 12:26:23 -0400

    Add missing file.

---
+++ src/gcp.rs
@@ -0,0 +1,23 @@
+use crate::config;
+
+pub fn invalidate_cdn_cache(path: &str) {
+    use std::process::Command;
+    let url_maps = &config::CONFIG.gcp.cdn_url_maps;
+
+    for url_map in url_maps.iter() {
+        Command::new("gcloud")
+            .args(&[
+                "compute",
+                "url-maps",
+                "invalidate-cdn-cache",
+                url_map,
+                "--path",
+                path,
+                "--project",
+                &config::CONFIG.gcp.project,
+                "--async",
+            ])
+            .spawn()
+            .expect("failed to execute gcloud");
+    }
+}
