13792462556cd8a258a25eb0aa40a4df8b33bb22
Brenden Matthews <brenden@diddyinc.com>
2019-08-07 18:39:55 -0400

    Add missing file.

---
+++ src/elasticsearch.rs
@@ -0,0 +1,51 @@
+extern crate elastic;
+
+use elastic::client::prelude::*;
+
+use crate::config;
+use crate::models;
+
+impl From<models::UpdateClientResponse> for ClientProfileDocument {
+    fn from(client: models::UpdateClientResponse) -> Self {
+        Self {
+            client_id: client.client_id,
+            full_name: client.full_name,
+            handle: client.handle,
+        }
+    }
+}
+
+#[derive(ElasticType, Serialize, Deserialize)]
+pub struct ClientProfileDocument {
+    #[elastic(client_id)]
+    pub client_id: String,
+    pub full_name: String,
+    pub handle: Option<String>,
+}
+
+pub struct ElasticSearchClient {
+    client: SyncClient,
+}
+
+impl ElasticSearchClient {
+    pub fn new() -> Self {
+        let builder =
+            SyncClientBuilder::new().sniff_nodes(config::CONFIG.elasticsearch.url.as_str());
+        Self {
+            client: builder.build().unwrap(),
+        }
+    }
+
+    pub fn update(&self, doc: ClientProfileDocument) {
+        self.client
+            .document::<ClientProfileDocument>()
+            .put_mapping()
+            .send()
+            .expect("failed to put document mapping");
+        self.client
+            .document()
+            .index(doc)
+            .send()
+            .expect("failed to index doc");
+    }
+}
