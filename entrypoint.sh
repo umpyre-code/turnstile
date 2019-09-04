e438c91f61a8d3d4d8b27679e7da659b778b8e10
Brenden Matthews <brenden@diddyinc.com>
2019-09-03 22:08:15 -0400

    Fix entrypoint script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,4 @@ set -x
 envsubst < /etc/config/Turnstile.toml.in > Turnstile.toml
 envsubst < /etc/config/Rocket.toml.in > Rocket.toml
 
-# Active gcloud service account
-gcloud auth activate-service-account --key-file=/etc/secrets/gcloud-sa.json
-
 exec turnstile "$@"
